a6d99bcd641dfbc6d4f1a692660fd94af245b625
Sloththeswift <71036969+Sloththeswift@users.noreply.github.com>
2021-11-10 13:40:43 +0100

    Add project files.

---
+++ Grupparbete Marshall/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grupparbete_Marshall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
