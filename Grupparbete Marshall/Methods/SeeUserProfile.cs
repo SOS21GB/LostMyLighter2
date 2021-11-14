20fc3608bf5898a6af58ddaa961d1dfccc9337d8
Aylin Canpunar <aylin.canpunar@elev.nbi-handelsakademin.se>
2021-11-14 13:10:09 +0100

    Print User Profile From ID

--- Grupparbete Marshall/Methods/SeeUserProfile.cs
+++ Grupparbete Marshall/Methods/SeeUserProfile.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Grupparbete_Marshall.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grupparbete_Marshall.Lists;
+
 
 namespace Grupparbete_Marshall.Methods
 {
     class SeeUserProfile
     {
+        public static User GetUser(int id)
+        {
+            User foundUser = UserList.userList.Find(user => user.Id == id);
+            return foundUser;
+        }
+        public static void PrintUser(int id)
+        {
+            User user = GetUser(id);
+            Console.WriteLine(user.Id);
+            Console.WriteLine(user.Name);
+            Console.WriteLine(user.Age);
+                
+             
+        }
+        
+        
+
+
+
+
     }
+
+
 }
