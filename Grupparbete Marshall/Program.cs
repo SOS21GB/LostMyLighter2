7d85f281b17e460bf364e8c174458229f08c7c61
Morzun <90629491+Morzun@users.noreply.github.com>
2021-11-16 09:23:48 +0100

    Startmeny (#12)
    Co-authored-by: Sloththeswift <71036969+Sloththeswift@users.noreply.github.com>

--- Grupparbete Marshall/Program.cs
+++ Grupparbete Marshall/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Grupparbete_Marshall
 {
@@ -6,8 +7,12 @@ namespace Grupparbete_Marshall
     {
         static void Main(string[] args)
         {
-            Lists.UserList.InitializeUserList();
-            
+
+            Menus.Startmenu start = new Menus.Startmenu();
+            start.Start();
+          
+            Lists.UserList.InitializeUserList();            
+
         }
     }
 }
