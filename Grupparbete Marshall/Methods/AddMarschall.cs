fca0b645731279e9faed04b7ea626e7b923b6fec
Wilma Hammarshall <89970576+Hammarshall@users.noreply.github.com>
2021-11-17 10:46:24 +0100

    Wilma metod add m (#17)
    * hrj
    
    * * AddMarschall.cs: Början på Add marshall
    
    * AddUser.cs:
    
    Co-authored-by: MinBek <93261992+MinBek@users.noreply.github.com>

--- Grupparbete Marshall/Methods/AddMarschall.cs
+++ Grupparbete Marshall/Methods/AddMarschall.cs
@@ -7,6 +7,33 @@ using System.Threading.Tasks;
 namespace Grupparbete_Marshall.Methods
 {
     class AddMarschall
+        //marshall id
+        //märke
+        //Brinntid ca
+        //adress
+        //tid reg
+        //anv som reg
+        //när slockna
     {
+       public static void AddMarshalls()
+       {
+            //Classes.Marschall marschall = new Classes.Marschall();
+
+            //marshall id saknas
+
+            Console.Write("Ange Marshallens märke: ");
+
+            Console.Write("Ange Marshallens Brinntid: ");
+
+            Console.Write("Ange gatuadressen för Marshallen: ");
+
+            Console.Write("Ange gatunummret för Marshallen: ");
+
+            //tid för reg, datetime
+
+            //anv reggade
+
+            Console.Write("Marshallen förväntas slockna ");
+        }
     }
 }
