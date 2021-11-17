a07b6f589b85b84b0efd573c094ddba3f0a6ba77
aycaan <89972310+aycaan@users.noreply.github.com>
2021-11-17 10:47:00 +0100

    Aylin (#18)
    * Print User Profile From ID
    
    * uppdatert fil
    
    Co-authored-by: Aylin Canpunar <aylin.canpunar@elev.nbi-handelsakademin.se>
    Co-authored-by: Wilma Hammarshall <89970576+Hammarshall@users.noreply.github.com>

--- Grupparbete Marshall/Program.cs
+++ Grupparbete Marshall/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using Grupparbete_Marshall.Classes;
+using System;
+using System;
 using static System.Console;
 
 namespace Grupparbete_Marshall
@@ -6,13 +8,18 @@ namespace Grupparbete_Marshall
     class Program
     {
         static void Main(string[] args)
+       
         {
+            Lists.UserList.InitializeUserList();
 
             Menus.Startmenu start = new Menus.Startmenu();
             start.Start();
           
             Lists.UserList.InitializeUserList();            
-
         }
+
     }
+
+
+
 }
