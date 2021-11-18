9de4c03da3402f0c9fad3b9d57e974d117ac9eb0
Sloththeswift <71036969+Sloththeswift@users.noreply.github.com>
2021-11-18 08:42:57 +0100

    Städat kod

--- Grupparbete Marshall/Methods/LoginUser.cs
+++ Grupparbete Marshall/Methods/LoginUser.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grupparbete_Marshall.Classes;
 
 namespace Grupparbete_Marshall.Methods
 {
     class LoginUser
     {
+        public static User currentUser = new User();
+        public static void UserLogin()
+        {
+
+            Console.WriteLine("Please enter your user ID: ");
+            int id = int.Parse(Console.ReadLine());
+
+
+            currentUser = User.GetUserById(id);
+
+            //Menus.LoginMenu.StartLoginMenu(); Mintamir
+
+        }
+
+
+
     }
 }
