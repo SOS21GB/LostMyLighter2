672e700827c6f767c30490a3a89a37d91ff72af9
Morzun <90629491+Morzun@users.noreply.github.com>
2021-11-19 11:44:21 +0100

    Mintamir2 (#28)
    * Endprogram
    
    * Endprogram
    
    Co-authored-by: Mintamir Ketema <mintamir.ketema@elev.nbi-handelsakademin.se>

--- Grupparbete Marshall/Methods/EndProgram.cs
+++ Grupparbete Marshall/Methods/EndProgram.cs
@@ -2,11 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+using Grupparbete_Marshall.Menus;
+
 
 namespace Grupparbete_Marshall.Methods
 {
     class EndProgram
     {
+        public static void ExitMarshall()
+        {
+
+
+            string text = @"
+______________ ______________    ___________ _______  ________ ._.
+\__    ___/   |   \_   _____/    \_   _____/ \      \ \______ \| |
+  |    | /    ~    \    __)_      |    __)_  /   |   \ |    |  \ |
+  |    | \    Y    /        \     |        \/    |    \|    `   \|
+  |____|  \___|_  /_______  /    /_______  /\____|__  /_______  /_
+                \/        \/             \/         \/        \/\/
+";
+
+            for (int i = 0; i < 5; i++)
+            {
+                WriteLine(text);
+                Thread.Sleep(500);
+                Clear();
+                Thread.Sleep(100);
+            }
+
+            WriteLine("\n Press any key to exit the program");
+            ReadKey(true);
+
+        }
+
+
     }
 }
