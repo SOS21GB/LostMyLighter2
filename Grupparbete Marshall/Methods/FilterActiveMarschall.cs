5ea1efd6a983f692d44a13b07631223ac4996295
Mintamir Ketema <mintamir.ketema@elev.nbi-handelsakademin.se>
2021-11-24 13:42:40 +0100

    structure

--- Grupparbete Marshall/Methods/FilterActiveMarschall.cs
+++ Grupparbete Marshall/Methods/FilterActiveMarschall.cs
@@ -3,23 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grupparbete_Marshall.Classes;
 
 namespace Grupparbete_Marshall.Methods
 {
     class FilterActiveMarschall
     {
-        public static void FilterActiveMarschall(string marschall)
-
-        int filterPostalCode = 0;
-
-        //    do
-        //    {
-        //        Console.WriteLine("v�lj postnummer");
-        //    } while (int.TryParse(Console.ReadLine(), out filterPostalCode));
-
-        //    foreach (Marschall m in all.Where(x => x.PostalCode == filterPostalCode))
-        //    {
-        //        Console.WriteLine(m.Print());
-        //    }
     }
 }
