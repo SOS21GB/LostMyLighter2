1341e4f3f24895534087d4aeb81ac88dfe859828
Aylin Canpunar <aylin.canpunar@elev.nbi-handelsakademin.se>
2021-11-25 10:36:33 +0100

    FoundMarschallScores

--- Grupparbete Marshall/Classes/User.cs
+++ Grupparbete Marshall/Classes/User.cs
@@ -163,6 +163,56 @@ namespace Grupparbete_Marshall.Classes
 
             Menus.MenuAtLogin.LoginMenu();
         }
+
+
+        public static void LighterHighScore()
+
+        {
+            User TopScores = new User();
+            foreach (var users in userList)
+            {
+                if (users == null)
+                {
+                    break;
+                }
+                if (users._lostLighters > TopScores._lostLighters)
+
+                {
+                    TopScores = users;
+                }
+            }
+
+            PrintUser(TopScores);
+        }
+
+        public static void FoundMaschallScore()
+        { 
+
+         User TopScores = new User();
+         foreach (var users in userList)
+         { 
+          
+              if (users == null) 
+             {  
+                break; 
+              }
+              if (users._foundMarschall > TopScores._foundMarschall)
+              {
+                    TopScores = users;
+              }
+        
+         } PrintUser(TopScores);
+         
+         }
+        
+     
+
+      
+            
+     }
+
+
+
     }
 
-}
+
